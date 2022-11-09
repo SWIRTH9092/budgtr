5c0c722bc82636b62f7b8314956a2f0c50763053
Your Name <you@example.com>
2022-11-09 13:13:13 -0500

    add 'get' to server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 const morgan = require("morgan")  // import Morgan (logging middleware)
 const PORT = process.env.POR || 3000; //(in case of env file missing)
 
+app.get('/', (req, res) => {
+    res.send(
+        '<h1>Get worked!</h1>'
+    );
+});
+
+
 app.listen(PORT, () => {
     console.log("listening on port:" , PORT)
 })
